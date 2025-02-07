6c454fd75933d0b49a8971fa3848f00c1b1c18a0
Nattapon Jutamas <nattaponj@outlook.com>
2025-02-07 21:29:24 +0700

    14. Overriding variables with .tfvars

---
+++ Section 3 - Input & output variables/terraform.tfvars
@@ -0,0 +1,4 @@
+resource_group_name     = "example-resources"
+resource_group_location = "west europe"
+app_service_plan_name   = "example-appserviceplan"
+app_service_name        = "nattapon-app-service"
--- Section 3 - Input & output variables/variables.tf
+++ Section 3 - Input & output variables/variables.tf
@@ -1,23 +1,19 @@
 variable "resource_group_name" {
-  default     = "example-resources"
   type        = string
   description = "value of the resource group name"
 }
 
 variable "resource_group_location" {
-  default     = "West Europe"
   type        = string
   description = "value of the resource group location"
 }
 
 variable "app_service_plan_name" {
-  default     = "example-appserviceplan"
   type        = string
   description = "value of the app service plan name"
 }
 
 variable "app_service_name" {
-  default     = "nattapon-app-service"
   type        = string
   description = "value of the app service name"
 }
