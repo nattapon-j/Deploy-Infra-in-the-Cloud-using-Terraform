7dbca0217f44d23b8f2d8322620ea32ecd84391f
Nattapon Jutamas <nattaponj@outlook.com>
2025-02-24 11:12:28 +0700

    24. Deploy Web App and SQL Database

--- Section 7 - Terraform Workspace/main.tf
+++ Section 7 - Terraform Workspace/main.tf
@@ -1,18 +1,75 @@
 provider "azurerm" {
+  version = "=3.0.0"
   features {}
-
 }
 
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-}
 
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
-  location = "southeastasia"
+  location = var.resource_group_location
+
+  tags = {
+    environment = "production"
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/app_service
+resource "azurerm_app_service_plan" "app_plan" {
+  name                = var.app_service_plan_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "webapp" {
+  name                = var.app_service_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.app_plan.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/mssql_database
+resource "azurerm_mssql_server" "sqldb" {
+  name                         = var.sql_server_name
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = var.sql_admin_login
+  administrator_login_password = var.sql_admin_password
+}
+
+resource "azurerm_mssql_database" "db" {
+  name           = "acctest-db-d"
+  server_id      = azurerm_mssql_server.sqldb.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 4
+  read_scale     = true
+  sku_name       = "BC_Gen5_2"
+  zone_redundant = false
+
+
+  tags = {
+    foo = "bar"
+  }
+
 }
--- Section 7 - Terraform Workspace/variables.tf
+++ Section 7 - Terraform Workspace/variables.tf
@@ -1,4 +1,39 @@
 variable "resource_group_name" {
-  type = string
+  type        = string
+  description = "Resource group name in Azure"
+}
 
+variable "resource_group_location" {
+    type        = string
+    description = "Resource group location in Azure"
+}
+
+variable "app_service_plan_name" {
+    type        = string
+    description = "App Service Plan name in Azure"
+}
+
+variable "app_service_name" {
+    type        = string
+    description = "App Service name in Azure"
+}
+
+variable "sql_server_name" {
+    type        = string
+    description = "SQL Server instance name in Azure"
+}
+
+variable "sql_database_name" {
+    type        = string
+    description = "SQL Database name in Azure"
+}
+
+variable "sql_admin_login" {
+    type        = string
+    description = "SQL Server admin login name in Azure" 
+}
+
+variable "sql_admin_password" {
+    type        = string
+    description = "SQL Server admin login password in Azure"
 }
